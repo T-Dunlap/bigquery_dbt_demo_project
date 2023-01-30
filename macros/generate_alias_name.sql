c4adf8fecbcaa76bf772924cec15ac273d17170e
Taylor Dunlap <taylor.dunlap@dbtlabs.com>
2023-01-30 23:09:17 +0000

    Added generate_schema_name and generate_alias_name

---
+++ macros/generate_alias_name.sql
@@ -0,0 +1,49 @@
+{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
+    {% do return(adapter.dispatch('generate_alias_name', 'dbt')(custom_alias_name, node)) %}
+{%- endmacro %}
+
+{% macro default__generate_alias_name(custom_alias_name=none, node=none) -%}
+
+    {%- if target.name == 'ci' -%}
+
+        {%- if custom_alias_name is none -%}
+
+            {{ target.schema }}_{{ node.name }}
+
+        {%- else -%}
+
+            {{ target.schema }}_{{ custom_alias_name | trim }}
+
+        {%- endif -%}
+
+    {%- elif target.name == 'dev' -%}
+
+        {%- if custom_alias_name is none -%}
+
+            {{ target.schema }}__{{ node.config.schema }}__{{ node.name }}
+
+        {%- else -%}
+
+            {{ target.schema }}__{{ node.config.schema }}__{{ (custom_alias_name | trim) }}
+
+        {%- endif -%}
+
+    {%- else  -%}
+
+        {%- if custom_alias_name is none -%}
+
+            {{ node.name }}
+
+        {%- else -%}
+
+            {{ custom_alias_name | trim }}
+
+        {%- endif -%}
+
+    {%- endif -%}
+
+
+
+
+
+{%- endmacro %}
